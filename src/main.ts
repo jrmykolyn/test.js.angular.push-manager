622ef7ba8824a475ac9b3be311c82583a47f4f3f
Jesse Mykolyn <jrmykolyn@gmail.com>
2019-09-21 00:06:36 -0400

    Compute and display user ID

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,12 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
+const getId = () => {
+  const a = [];
+  a.push(window.navigator.userAgent);
+  return a.join(':');
+};
+
 // Ensure that service workers are supported in the current browser.
 if ('navigator' in window && 'serviceWorker' in window.navigator) {
 
@@ -24,9 +30,16 @@ if ('navigator' in window && 'serviceWorker' in window.navigator) {
         userVisibleOnly: true,
         applicationServerKey: "BM0iVtYTuEGGCzJTufYOGcBrV101gIRuRzt5Q7Y1VsX47C3n-shwhz1CU6UfwH2Ij_QqAArjJhkNm0-onTUAh7o",
       }).then((subscription) => {
+
+        // Bundle the subscription data and a computed ID that corresponds to
+        // the user's defive into a new object; add data to DOM.
         const target = document.querySelector('pre');
+        const data = {
+          id: getId(),
+          subscription,
+        };
         if (target) {
-          target.innerHTML = JSON.stringify(subscription, null, 2);
+          target.innerHTML = JSON.stringify(data, null, 2);
         }
       })
       .catch((err) => {
