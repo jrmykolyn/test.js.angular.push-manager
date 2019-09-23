de6c982a7a584039c9e65c59b021a0ec74d8d9de
Jesse Mykolyn <jrmykolyn@gmail.com>
2019-09-22 22:49:40 -0400

    Consolidate key-fetching and service worker flows

--- src/main.ts
+++ src/main.ts
@@ -27,51 +27,41 @@ if ('navigator' in window && 'serviceWorker' in window.navigator) {
     .then((keys) => {
       const [{ publicKey }] = keys;
       window['__APPLICATION_SERVER_KEY__'] = publicKey;
-    })
-    .catch((err) => {
-      console.error('Whoops, something went wrong!');
-      console.error(err);
-    });
 
-  // Register the application service worker, exposing the service worker
-  // reference.
-  window.navigator.serviceWorker.register('/assets/service-worker.js')
+      // Register the application service worker, exposing the service worker
+      // reference.
+      return window.navigator.serviceWorker.register('/assets/service-worker.js');
+    })
     .then((serviceWorker) => {
 
       // Request push notification permission, passing in the public VAPID key.
-      serviceWorker.pushManager.subscribe({
+      return serviceWorker.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey: "BM0iVtYTuEGGCzJTufYOGcBrV101gIRuRzt5Q7Y1VsX47C3n-shwhz1CU6UfwH2Ij_QqAArjJhkNm0-onTUAh7o",
-      }).then((subscription) => {
+      });
+    })
+    .then((subscription) => {
 
-        // Bundle the subscription data and a computed ID that corresponds to
-        // the user's device into a new object; add data to DOM.
-        const target = document.querySelector('pre');
-        const data = {
-          id: getId(),
-          subscription,
-        };
-        if (target) {
-          target.innerHTML = JSON.stringify(data, null, 2);
-          window.fetch('http://localhost:4600/subscriptions', {
-            method: 'POST',
-            body: JSON.stringify(data),
-          })
-            .then((response) => {
-              console.log(response);
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        }
-      })
-      .catch((err) => {
-        console.error('__ WHOOPS, FAILED TO RETRIEVE SUBSCRIPTION DATA');
-        console.error(err);
+      // Bundle the subscription data and a computed ID that corresponds to
+      // the user's device into a new object; add data to DOM.
+      const target = document.querySelector('pre');
+      const data = {
+        id: getId(),
+        subscription,
+      };
+
+      if (!target) {
+        return Promise.reject(new Error('Document does not contain elem.'));
+      }
+
+      target.innerHTML = JSON.stringify(data, null, 2);
+      return window.fetch('http://localhost:4600/subscriptions', {
+        method: 'POST',
+        body: JSON.stringify(data),
       });
     })
     .catch((err) => {
-      console.error('__ WHOOPS, SOMETHING WENT WRONG!');
+      console.error('__ WHOOPS, FAILED TO RETRIEVE SUBSCRIPTION DATA');
       console.error(err);
     });
 }
