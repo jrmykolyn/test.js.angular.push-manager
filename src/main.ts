03c7a9b0c535e0dae54ad90d9f7070d8e8f25476
Jesse Mykolyn <jrmykolyn@gmail.com>
2019-09-22 23:00:51 -0400

    Update comments

--- src/main.ts
+++ src/main.ts
@@ -20,48 +20,61 @@ const getId = () => {
 // Ensure that service workers are supported in the current browser.
 if ('navigator' in window && 'serviceWorker' in window.navigator) {
 
+  // 1.
+  // Fetch the public VAPID key.
   window.fetch('http://localhost:4600/keys')
     .then((response) => {
       return response.json();
     })
+    // 2.
+    // Expose the parsed VAPID key as a property of the window.
+    // Register the application service worker.
     .then((keys) => {
       const [{ publicKey }] = keys;
       window['__APPLICATION_SERVER_KEY__'] = publicKey;
-
-      // Register the application service worker, exposing the service worker
-      // reference.
       return window.navigator.serviceWorker.register('/assets/service-worker.js');
     })
+    // 3.
+    // Request push notification permission using the service worker reference
+    // and the VAPID key.
     .then((serviceWorker) => {
-
-      // Request push notification permission, passing in the public VAPID key.
       return serviceWorker.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey: "BM0iVtYTuEGGCzJTufYOGcBrV101gIRuRzt5Q7Y1VsX47C3n-shwhz1CU6UfwH2Ij_QqAArjJhkNm0-onTUAh7o",
       });
     })
+    // 4.
+    // Compute an identifier for the current user/user's device.
+    // Bundle subscription data and computed ID.
+    // Display subscription data and computed ID via the DOM.
     .then((subscription) => {
-
-      // Bundle the subscription data and a computed ID that corresponds to
-      // the user's device into a new object; add data to DOM.
       const target = document.querySelector('pre');
-      const data = {
-        id: getId(),
-        subscription,
-      };
+      const data = { id: getId(), subscription };
 
       if (!target) {
         return Promise.reject(new Error('Document does not contain elem.'));
       }
 
       target.innerHTML = JSON.stringify(data, null, 2);
+
+      return new Promise((resolve) => resolve(data));
+    })
+    // 5.
+    // Send subscription data to back-end so that it can be persisted.
+    .then((data) => {
       return window.fetch('http://localhost:4600/subscriptions', {
         method: 'POST',
         body: JSON.stringify(data),
       });
     })
+    // 6.
+    // Handle errors, including:
+    // - Failure to register service worker.
+    // - Failure to extract subscription data.
+    // - Failure to display subscription data.
+    // - Failed to POST subscription data.
     .catch((err) => {
-      console.error('__ WHOOPS, FAILED TO RETRIEVE SUBSCRIPTION DATA');
+      console.error('__ WHOOPS, SOMETHING WENT WRONG!');
       console.error(err);
     });
 }
