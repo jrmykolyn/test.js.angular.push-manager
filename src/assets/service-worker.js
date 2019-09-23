1993b01fad9fa189b4e65e6cfe39e86d52613791
Jesse Mykolyn <jrmykolyn@gmail.com>
2019-09-22 22:47:49 -0400

    Update service worker to use options

--- src/assets/service-worker.js
+++ src/assets/service-worker.js
@@ -1,9 +1,12 @@
 self.addEventListener('push', (event) => {
   console.log('__ RECEIVED PUSH EVENT');
   if (event.data) {
-    const data = event.data.json();
+    const data = (event.data.json() || {});
+    const { title = 'Default Title', options = {} } = data;
+
     this.registration.showNotification(
-      (data.title || 'Default Title')
+      title,
+      options
     );
   }
 });
